67ffdbeff35191ecc595a4332424b069fe460fd3
guiemi <guiemi.teixeira@gmail.com>
2019-01-23 17:08:30 -0200

    WIP: savegame

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,2 +1,25 @@
-def is_pangram(sentence):
-    pass
+import string
+
+def is_pangram(*sentences):
+    for sentence in sentences:
+        if sentence == "":  # sentence_empty
+            return False
+        
+        
+        else:  # Se sentence não for vazia
+            for char in sentence:
+                if "x" not in sentence:  # missing_character_x
+                    return False
+
+                if char not in string.ascii_letters:  # another_missing_character
+                    return False  # Não é um pangram
+
+                else: # É um pangram
+                    if sentence.islower():  # perfect_lower_case_pangram
+                        return True
+                    
+                    if string.punctuation in sentence:
+                        return True
+
+is_pangram()
+is_pangram('abcdefghijklmnopqrstuvwxyz')
