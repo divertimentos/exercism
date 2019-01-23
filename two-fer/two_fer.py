d15070970b0a1485db526738f03689109facdd5b
guiemi <guiemi.teixeira@gmail.com>
2019-01-23 14:32:17 -0200

    Finaliza o exercício 2/111

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,2 +1,17 @@
-def two_fer(name):
-    pass
+def two_fer(*names):
+    
+    if len(names) == 0:  # se chamar a função sem nenhum argumento
+        name = "you"
+        return f"One for {name}, one for me."
+    else:
+        for name in names:
+            if name == "Alice":
+                return f"One for {name}, one for me."
+            elif name == "Bob":
+                return f"One for {name}, one for me."
+            else:
+                raise Exception("Nome inválido")
+two_fer()
+two_fer("Alice")
+two_fer("Bob")
+# two_fer("Chris")
