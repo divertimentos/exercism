8984eb69a5c59d489aa6ca2661b82dd789fc4849
guiemi <guiemi.teixeira@gmail.com>
2019-01-23 14:45:07 -0200

    Comenta o código

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -3,8 +3,8 @@ def two_fer(*names):
     if len(names) == 0:  # se chamar a função sem nenhum argumento
         name = "you"
         return f"One for {name}, one for me."
-    else:
-        for name in names:
+    else:  # Se houver pelo menos um argumento:
+        for name in names:  # "*names" é uma lista
             if name == "Alice":
                 return f"One for {name}, one for me."
             elif name == "Bob":
