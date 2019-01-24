d3110fe2c897e2dde9479a8b8c4bcbcac4ca882a
guiemi <guiemi.teixeira@gmail.com>
2019-01-24 14:55:00 -0200

    Limpa o código para o submit

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -4,18 +4,17 @@ import string
 def is_pangram(*sentences):
     for sentence in sentences:
 
-        if len(sentence) == 0:  # se sentence for vazia
+        if len(sentence) == 0:
             return False
-
-        not_in_sentence = list()
-        for letter in string.ascii_lowercase:
-            # se uma letra do alfabeto não estiver na sentença:
-            if letter not in sentence.lower():
-                not_in_sentence.append(letter)  # adiciona a letra à lista
-
-        if len(not_in_sentence) == 0:  # se não houver letra de fora do alfabeto
-            print("It's a pangram!")
-            return True
         else:
-            print("It's not a pangram")
-            return False
+            not_in_sentence = list()
+            for letter in string.ascii_lowercase:
+                if letter not in sentence.lower():
+                    not_in_sentence.append(letter)
+
+            if len(not_in_sentence) == 0:
+                print("It's a pangram!")
+                return True
+            else:
+                print("It's not a pangram")
+                return False
