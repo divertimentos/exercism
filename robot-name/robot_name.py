12ec7faaa441b213ace51eaa6ec481c5697dd097
guiemi <guiemi.teixeira@gmail.com>
2019-01-24 16:58:28 -0200

    WIP: save game

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,3 +1,10 @@
 class Robot(object):
-    def __init__(self):
-        pass
+    def __init__(self, char, num):
+        self.char = "abc"
+        self.num = "123"
+
+    def generate_char(self):
+
+        
+
+# droid = Robot()
