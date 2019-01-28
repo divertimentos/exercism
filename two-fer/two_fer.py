fd379d3ac758a99dc484ea6106dbd3cb5d260852
guiemi <guiemi.teixeira@gmail.com>
2019-01-28 10:46:02 -0200

    Reescreve a função: economia de código.

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,17 +1,2 @@
-def two_fer(*names):
-    
-    if len(names) == 0:  # se chamar a função sem nenhum argumento
-        name = "you"
-        return f"One for {name}, one for me."
-    else:  # Se houver pelo menos um argumento:
-        for name in names:  # "*names" é uma lista
-            if name == "Alice":
-                return f"One for {name}, one for me."
-            elif name == "Bob":
-                return f"One for {name}, one for me."
-            else:
-                raise Exception("Nome inválido")
-two_fer()
-two_fer("Alice")
-two_fer("Bob")
-# two_fer("Chris")
+def two_fer(name = "you"):
+    return f"One for {name}, one for me."
