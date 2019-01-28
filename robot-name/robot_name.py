32f3630920f516a3e8261790d74657546b976be5
guiemi <guiemi.teixeira@gmail.com>
2019-01-28 16:54:41 -0200

    WIP: savegame

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,10 +1,24 @@
+import string
+import random
+from random import randint
+
+
 class Robot(object):
     def __init__(self, char, num):
-        self.char = "abc"
-        self.num = "123"
+        self.char = char.upper()
+        self.num = num
 
-    def generate_char(self):
+    def random_name(self, name_length=3):
+        letters = string.ascii_uppercase
+        return ''.join(random.choice(letters) for i in range(name_length))
 
-        
+    def random_num(self, num_length=3):
+        lista = list()
 
-# droid = Robot()
+        for i in range(num_length):
+            x = randint(0, 9)
+            lista.append(x)
+        return ','.join(str(lista))
+
+print(Robot.random_name(3))
+print(Robot.random_num(3))
