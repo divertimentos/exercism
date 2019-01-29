9512e7b16da142d68cf3567cb0fb2d43e9eb946d
guiemi <guiemi.teixeira@gmail.com>
2019-01-29 14:02:55 -0200

    Gera chars e nums aleatórios pra comporem o nome do robô

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -10,15 +10,20 @@ class Robot(object):
 
     def random_name(self, name_length=3):
         letters = string.ascii_uppercase
-        return ''.join(random.choice(letters) for i in range(name_length))
-
+        name = ''.join(random.choice(letters) for i in range(name_length))
+        return name
     def random_num(self, num_length=3):
         lista = list()
 
         for i in range(num_length):
             x = randint(0, 9)
             lista.append(x)
-        return ','.join(str(lista))
+        
+        lista = ''.join(map(str, lista))
+        return lista
+
+    def join():
+        pass
 
 print(Robot.random_name(3))
 print(Robot.random_num(3))
