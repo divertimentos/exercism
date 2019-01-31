ca257ca27ca59636981142cd124d927c00b9c6f2
guiemi <guiemi.teixeira@gmail.com>
2019-01-31 12:01:13 -0200

    Finaliza o exercício

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,15 @@
 def is_armstrong(number):
-    pass
+    numbers_list = [int(x) for x in str(number)]  # turn numbers into a list
+    length = len(numbers_list)
+    powered_numbers = list()
+
+    for digit in numbers_list:
+        powered_numbers.append(digit**length)
+    
+    result = sum(powered_numbers)
+    if result == number:
+        print(f"{number} é um número de Armstrong!")
+        return True
+    else:
+        print(f"{number} não é um número de Armstrong!")
+        return False
