f444df638c0f030531201bf9154d4efde18a362c
guiemi <guiemi.teixeira@gmail.com>
2019-01-31 16:20:58 -0200

    WIP: savegame

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,2 +1,9 @@
 def proteins(strand):
-    pass
+    if len(strand) % 3 == 0:
+
+        for i in range(0, len(strand),3):
+            pass
+    else:
+        print("Invalid RNA sequence")
+
+proteins("123456")
