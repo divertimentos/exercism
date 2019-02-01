969179f258733710f5eb179c87350d4daf1081f9
guiemi <guiemi.teixeira@gmail.com>
2019-02-01 16:56:13 -0200

    WIP: savegame

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,11 +1,40 @@
 def proteins(strand):
         strand = list(strand)
         codons = list()
+        sequence = list()
         while len(strand) > 0:
-                codon = strand[0:3]
-                codons.append(codon)
-                strand = strand[3:]
-                # print(codons)
-        print(codons)        
+                codons.append(strand[:3])  # adiciona a codons os primeiros 3 itens de strand
+                strand = strand[3:]  # remove da lista os primeiros 3 itens
+        
+        for base in codons:  # para cada lista na lista codons
+                base = "".join(map(str, base))  # transforma base em uma string
+                sequence.append(base)  # adiciona a nova string à lista 'sequence'
 
-proteins("AAABBBCCC")
+        # print(sequence)
+
+        aminoacids = {
+                "Methionine": ["AUG"],
+                "Phenylalanine": ["UUU", "UUC"],
+                "Leucine": ["UUA", "UUG"],
+                "Serine": ["UCU", "UCC", "UCA", "UCG"],
+                "Cysteine": ["UGU", "UGC"],
+                "Tryptophan": ["UGG"],
+                "STOP": ["UAA", "UAG", "UGA"],
+        }
+
+        
+
+        # print(sequence)
+        
+        # for s in sequence:
+        #         print(type(s))
+
+        # for k, v in aminoacids.items():
+        #         print(type(v))
+
+        # for s in sequence:
+        #         for k, v in aminoacids.items():
+        #                 for i in v:
+        #                         if i == s:
+        #                                 print(k)
+proteins("AUGUUUUCUUAAAUG")
