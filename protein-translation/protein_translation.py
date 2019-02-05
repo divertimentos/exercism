3f99fc2583c90e7f7b3af59c8b6bb945fb9bbd78
guiemi <guiemi.teixeira@gmail.com>
2019-02-05 14:21:44 -0200

    WIP: código está printando os aminoácidos corretamente.
    Agora falta impedi-lo de continuar printando caso encontre uma condição
    de parada

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,40 +1,70 @@
 def proteins(strand):
-        strand = list(strand)
-        codons = list()
-        sequence = list()
-        while len(strand) > 0:
-                codons.append(strand[:3])  # adiciona a codons os primeiros 3 itens de strand
-                strand = strand[3:]  # remove da lista os primeiros 3 itens
-        
-        for base in codons:  # para cada lista na lista codons
-                base = "".join(map(str, base))  # transforma base em uma string
-                sequence.append(base)  # adiciona a nova string à lista 'sequence'
+    strand = list(strand)
+    codons = list()
+    sequence = list()
+    while len(strand) > 0:
+        # adiciona a codons os primeiros 3 itens de strand
+        codons.append(strand[:3])
+        strand = strand[3:]  # remove da lista os primeiros 3 itens
 
-        # print(sequence)
+    for base in codons:  # para cada lista na lista codons
+        base = "".join(map(str, base))  # transforma base em uma string
+        sequence.append(base)  # adiciona a nova string à lista 'sequence'
 
-        aminoacids = {
-                "Methionine": ["AUG"],
-                "Phenylalanine": ["UUU", "UUC"],
-                "Leucine": ["UUA", "UUG"],
-                "Serine": ["UCU", "UCC", "UCA", "UCG"],
-                "Cysteine": ["UGU", "UGC"],
-                "Tryptophan": ["UGG"],
-                "STOP": ["UAA", "UAG", "UGA"],
-        }
+    # sequence --> ['AUG', 'UUU', 'UCU', 'UAA', 'AUG']
 
-        
+    aminoacids = {
+        "Methionine": ["AUG"],
+        "Phenylalanine": ["UUU", "UUC"],
+        "Leucine": ["UUA", "UUG"],
+        "Serine": ["UCU", "UCC", "UCA", "UCG"],
+        "Cysteine": ["UGU", "UGC"],
+        "Tryptophan": ["UGG"],
+    }
 
-        # print(sequence)
-        
-        # for s in sequence:
-        #         print(type(s))
+    stop = ["UAA", "UAG", "UGA"]
+    
 
-        # for k, v in aminoacids.items():
-        #         print(type(v))
+    # print(sequence)
 
-        # for s in sequence:
-        #         for k, v in aminoacids.items():
-        #                 for i in v:
-        #                         if i == s:
-        #                                 print(k)
+    for key, value in aminoacids.items():
+        for i in value:
+            if i in sequence:
+                print(key)
+    
+
+    # for stop_condition in stop:
+    #     for key, value in aminoacids.items():
+    #         for i in value:
+    #             for s in sequence:
+    #                 if stop_condition == s:
+    #                     print("irá parar")
+    #                     break
+    #                 if s in i:
+    #                     print(key)
+
+
+
+
+    # for key, value in aminoacids.items():
+    #     for i in value:
+    #         for s in sequence:
+    #             if aminoacids.get("STOP") == s:
+    #                 print("teste")
+
+                # if i in s:
+                #     print(key)
+    # print(sequence)
+
+    # for s in sequence:
+    #         print(type(s))
+
+
+    # for k, v in aminoacids.items():
+    #         print(type(v))
+    # for s in sequence:
+    #         for k, v in aminoacids.items():
+    #                 for i in v:
+    #                         if i == s:
+    #                                 print(k)
 proteins("AUGUUUUCUUAAAUG")
