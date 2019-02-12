6f65a30f741f0f37dc6422ed8d9e0553a35585ab
guiemi <guiemi.teixeira@gmail.com>
2019-02-12 15:08:01 -0200

    Implementa mais três métodos:
    highest score from the list
    the last added score
    the three highest scores
    
    e só define o último método do exercício

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -20,10 +20,28 @@ class HighScores:
                 self.scores.append(random_int)
         return self.scores
 
+    # Highest score from the list:
     def highest_score(self):
         return max(self.scores)
 
+    # the last added score:
+    def last_added(self):
+        return self.scores[-1]
+
+    # the three highest scores:
+    def three_highest(self):
+        sorted_scores = sorted(self.scores)
+        return sorted_scores[-3:]
+
+    # the difference between the last and the highest scores:
+    def last_x_highest(self):
+        pass
 
 player = HighScores(10)
-print(player.generate_scores(5))
-print(player.highest_score())
+
+print(f"Generate score list: {player.generate_scores(10)}")
+
+# print(f"Highest score: {player.highest_score()}")
+# print(f"The last added score: {player.last_added()}")
+
+print(f"Three highest scores: {player.three_highest()}")
