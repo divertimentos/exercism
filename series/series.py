58847d13c1c5d2a4993475c7e91be4313d01ba9d
guiemi <guiemi.teixeira@gmail.com>
2019-03-01 16:41:37 -0300

    Cria uma função que gera aleatoriamente uma sequência de números
    inteiros de acordo com um length passado como parâmetro

--- series/series.py
+++ series/series.py
@@ -1,2 +1,12 @@
+import random
+
+def generate_series(length):
+    series_list = list()
+    while len(series_list) < length:
+        series_list.append(random.randint(0, 9))
+    return ''.join(map(str, series_list))
+
 def slices(series, length):
     pass
+
+
