d616352e8379c11695b0ce7aeb94b61db42f5c48
guiemi <guiemi.teixeira@gmail.com>
2019-04-05 18:08:09 -0300

    WIP: faz funcionar um slice personalizado que percorre a lista conforme o parâmetro dado na função

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -1,18 +1,23 @@
-"""Given a string of digits, calculate the largest product 
-for a contiguous substring of digits of length n.
+"""
+Given a string of digits, calculate the largest product for a contiguous substring of digits of length n.
 
-For example, for the input '1027839564', 
-the largest product for a series of 3 digits is 270 (9 * 5 * 6), 
+For example, for the input '1027839564', the largest product for a series of 3 digits is 270 (9 * 5 * 6), 
 and the largest product for a series of 5 digits is 7560 (7 * 8 * 3 * 9 * 5).
 
-Note that these series are only required to occupy adjacent positions
-in the input; 
+Note that these series are only required to occupy adjacent positions in the input; 
 the digits need not be numerically consecutive.
 
-For the input 
-'73167176531330624919225119674426574742355349194934', 
+For the input '73167176531330624919225119674426574742355349194934', 
 the largest product for a series of 6 digits is 23520.
+
 """
 
 def largest_product(series, size):
-    pass
+    counter = 0
+    for i in series:
+        print(series[counter:size])
+        size += 1
+        counter += 1
+
+
+print(largest_product("12345", 3))
