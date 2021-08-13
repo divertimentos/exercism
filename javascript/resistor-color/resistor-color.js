80311cdcc3dcf71522e5afe915120763f3a50f35
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2021-08-13 18:09:22 -0300

    Adds the first 3 solved exercises

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,17 @@
+export const colorCode = (color) => {
+  const colors = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+  ];
+  return colors.indexOf(color);
+};
+
+export const COLORS = undefined;
