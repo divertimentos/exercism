4bcbb50c746199f870f54c40d58d19690486a2d5
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2021-08-14 20:15:50 -0300

    Solves Gigasecond

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
+export const gigasecond = (startDate) => {
+  return new Date(startDate.getTime() + 1e12);
 };
