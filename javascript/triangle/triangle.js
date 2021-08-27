1cad952f52dc4fb75cf70013e45c031298adb56a
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2021-08-27 15:35:46 -0300

    2/3 of tests passed (equillateral and isosceles)

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,14 +1,37 @@
 export class Triangle {
   constructor(...sides) {
-    throw new Error("Remove this statement and implement this function");
+    this.sides = sides;
+    this.a = sides[0];
+    this.b = sides[1];
+    this.c = sides[2];
+  }
+
+  get sumSides() {
+    return this.sides.reduce((sum, curr) => sum + curr);
+  }
+
+  get isTriangle() {
+    return (
+      this.a + this.b > this.c &&
+      this.a + this.c > this.b &&
+      this.b + this.c > this.a
+    );
   }
 
   get isEquilateral() {
-    throw new Error("Remove this statement and implement this function");
+    // return this.sumSides % this.sides.length == 0 && this.sumSides / this.sides.length == 0;
+    return this.sumSides > 0 && this.a === this.b && this.b === this.c;
   }
 
   get isIsosceles() {
-    throw new Error("Remove this statement and implement this function");
+    if (this.isTriangle) {
+      return (
+        this.sides.sort()[0] == this.sides.sort()[1] ||
+        this.sides.sort()[1] == this.sides.sort()[2]
+      );
+    }
+
+    return false;
   }
 
   get isScalene() {
