68512cb2c68d06190ef4fa04da79f851b2c5687f
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2021-09-03 12:28:57 -0300

    WIP

--- javascript/freelancer-rates/freelancer-rates.js
+++ javascript/freelancer-rates/freelancer-rates.js
@@ -1,24 +1,4 @@
 // @ts-check
-//
-// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion on the web
-// and supported IDEs when implementing this exercise. You don't need to
-// understand types, JSDoc, or TypeScript in order to complete this JavaScript
-// exercise, and can completely ignore this comment block and directive.
-
-// 👋🏽 Hi again!
-//
-// A quick reminder about exercise stubs:
-//
-// 💡 You're allowed to completely clear any stub before you get started. Often
-// we recommend using the stub, because they are already set-up correctly to
-// work with the tests, which you can find in ./freelancer-rates.spec.js.
-//
-// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
-// in idiomatic JavaScript, but some companies and style-guides do enforce them.
-//
-// Get those rates calculated!
-
 /**
  * The day rate, given a rate per hour
  *
@@ -26,7 +6,7 @@
  * @returns {number} the rate per day
  */
 export function dayRate(ratePerHour) {
-  throw new Error('Implement the dayRate function');
+  return ratePerHour * 8;
 }
 
 /**
@@ -37,7 +17,11 @@ export function dayRate(ratePerHour) {
  * @returns {number} the rounded up monthly rate
  */
 export function monthRate(ratePerHour, discount) {
-  throw new Error('Implement the monthRate function');
+   const newDiscount = discount === 0 ? 1 : discount
+  //  console.log("DESCONTOOOOOOOOOOO: ", newDiscount)
+  //  console.log("RATE PER HOUR: ", ratePerHour)
+  //  console.log("dayRate: ", dayRate(ratePerHour))
+  return Math.round((ratePerHour * 8) * 22 * newDiscount)
 }
 
 /**
@@ -49,7 +33,7 @@ export function monthRate(ratePerHour, discount) {
  * @returns {number} the number of days
  */
 export function daysInBudget(budget, ratePerHour, discount) {
-  throw new Error('Implement the daysInBudget function');
+  throw new Error("Implement the daysInBudget function");
 }
 
 /**
@@ -60,5 +44,5 @@ export function daysInBudget(budget, ratePerHour, discount) {
  * @returns {number} the discounted value
  */
 function applyDiscount(value, percentage) {
-  throw new Error('Implement the applyDiscount function');
+  throw new Error("Implement the applyDiscount function");
 }
