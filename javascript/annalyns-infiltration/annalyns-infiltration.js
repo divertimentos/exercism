8194d61e1a63d875e58c68786e96a85c6bf24619
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2022-01-08 23:30:57 -0300

    Solves the exercise

--- javascript/annalyns-infiltration/annalyns-infiltration.js
+++ javascript/annalyns-infiltration/annalyns-infiltration.js
@@ -27,7 +27,7 @@
  * @return {boolean} Whether or not you can execute a fast attack.
  */
 export function canExecuteFastAttack(knightIsAwake) {
-  throw new Error('Implement the canExecuteFastAttack function');
+  return !knightIsAwake;
 }
 
 /**
@@ -40,7 +40,7 @@ export function canExecuteFastAttack(knightIsAwake) {
  * @returns {boolean} Whether or not you can spy on someone.
  */
 export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
-  throw new Error('Implement the canSpy function');
+  return !!knightIsAwake || !!archerIsAwake || !!prisonerIsAwake;
 }
 
 /**
@@ -52,7 +52,7 @@ export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
  * @returns {boolean} Whether or not you can send a signal to the prisoner.
  */
 export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
-  throw new Error('Implement the canSignalPrisoner function');
+  return !!prisonerIsAwake && !archerIsAwake;
 }
 
 /**
@@ -71,5 +71,16 @@ export function canFreePrisoner(
   prisonerIsAwake,
   petDogIsPresent
 ) {
-  throw new Error('Implement the canFreePrisoner function');
+  if (!!petDogIsPresent && !archerIsAwake) {
+    return true;
+  } else if (
+    !petDogIsPresent &&
+    !!prisonerIsAwake &&
+    !archerIsAwake &&
+    !knightIsAwake
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
