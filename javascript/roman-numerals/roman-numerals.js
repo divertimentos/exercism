571d8db2fc58ccbc7ed1466c8db1e7864be4dbe5
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2022-01-22 20:43:14 -0300

    Solves this one looking at a YouTube video
    (Because I'm a complete loser)

---
+++ javascript/roman-numerals/roman-numerals.js
@@ -0,0 +1,33 @@
+//
+// This is only a SKELETON file for the 'Roman Numerals' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const toRoman = (num) => {
+  const romanNumbers = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+  let roman = "";
+
+  for (let key in romanNumbers) {
+    while (num >= romanNumbers[key]) {
+      roman += key;
+      num -= romanNumbers[key];
+    }
+  }
+
+  return roman;
+};
